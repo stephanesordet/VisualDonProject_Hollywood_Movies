d305778de077622c2412eff304072fdad8a108c9
Jules Sandoz <jules.sandoz@heig-vd.ch>
2022-03-29 11:30:42 +0200

    fixed scale and displayed circles per year with colors

--- code/src/index.js
+++ code/src/index.js
@@ -1,6 +1,9 @@
 import * as d3 from 'd3';
 import file from '../assets/movies.json'
 import * as Movies from './loadMovies';
+import {
+  moviesToCircles
+} from './loadMovies';
 const API_KEY = '24cd33d154dc43bf62799d9af836baa3'
 const API_QUERY = "https://api.themoviedb.org/3/search/movie?api_key=" + API_KEY + "&query="
 const POSTER_BASE_URL = 'https://image.tmdb.org/t/p/original';
@@ -9,76 +12,143 @@ const movieList = document.querySelector('.list')
 const movieListItemTemplate = document.getElementById('movieList').cloneNode(true);
 
 d3.select('body').append('h1').text('TEST')
+
+// drawGraph()
+
 const svgGraph = d3.select('body').append('svg').attr('class', 'graph')
 
+// moviesToCircles(Movies);
 
+const margin = {
+    top: 50,
+    right: 10,
+    bottom: 0,
+    left: 100
+  },
+  width = window.innerWidth * 0.7 - margin.left - margin.right,
+  height = window.innerHeight * 0.9 - margin.top - margin.bottom;
 
-// renderMovies(file);
-
-const margin = {top : 50, right: 10, bottom: 0, left: 100},
-		   width = window.innerWidth*0.7 - margin.left - margin.right,
-		   height = window.innerHeight*0.9 - margin.top - margin.bottom;
- 
 svgGraph.attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  	.append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+  .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 const x = d3.scaleLinear()
-		.domain([1976,2021])
-		.range([0,width])
- 
+  .domain([1977, 2021])
+  .range([10, width])
+
 svgGraph.append('g')
   .attr("transform", "translate(5," + height + ")")
-  .call(d3.axisTop(x).ticks(45).tickSize(10)).selectAll("text")  
-            .style("text-anchor", "end")
-            .attr("dx", "-0.6em")
-            .attr("dy", "2.4em")
-            .attr("transform", "rotate(-65)" );;
- 
-const y = d3.scaleLinear()
-		.domain([0,20])
-		.range([height,0+10])
- 
+  .call(d3.axisTop(x).ticks(44).tickSize(10)).selectAll("text")
+  .style("text-anchor", "end")
+  .attr("dx", "-0.6em")
+  .attr("dy", "2.4em")
+  .attr("transform", "rotate(-65)");;
+
+const y = d3.scalePow()
+  .exponent(0.5)
+  .domain([70000000, 2847246203])
+  .range([height, 0 + 10])
+
 svgGraph.append('g')
   .call(d3.axisRight(y).ticks(20));
 
-
-/**
- * It takes in a URL and returns the poster URL.
- * @param url - The URL of the API endpoint you want to request.
- * @returns The URL of the poster image.
- */
-async function loadJson(url) {
-    const response = await fetch(url)
-    const parsedJson = await response.json()
-    const posterURL = parsedJson.results[0].poster_path;
-    return posterURL;
-}
-
-/**
- * Given a list of movies, render each movie's title and poster image
- * @param movies - an array of movie objects
- */
-async function renderMovies(movies) {
-    for (const movie of movies) {
-        const movieTitle = movie.Title.slice(0, movie.Title.length - 7)
-        const moviePoster = await loadJson(API_QUERY + movieTitle.replaceAll(' ', '+'))
-        renderMovie(movieTitle, moviePoster)
+svgGraph.selectAll("circle")
+  .data(file)
+  .enter()
+  .append("circle")
+  .attr('cx', f => x(parseInt(f.Title.substring(f.Title.length - 5, f.Title.length-1))))
+  .attr('cy', f => y((f['World Sales (in $)'])))
+  .attr('r', 5)
+  .attr('fill',f => {
+    let themes = f.Genre.substring(
+      f.Genre.indexOf('[') + 1,
+      f.Genre.lastIndexOf(']')
+    )
+    let genre = themes.split(' ')
+    genre = genre[0].substring(
+      genre[0].indexOf("'") + 1,
+      genre[0].lastIndexOf("'")
+    )
+    switch (genre) {
+      case 'Action':
+        return 'indianred';
+        break;
+      case 'Adventure':
+        return 'blue';
+        break;
+      case 'Animation':
+        return 'green';
+        break;
+      case 'Comedy':
+        return 'yellow';
+        break;
+      case 'Crime':
+        return 'orange';
+        break;
+      case 'Documentary':
+        return 'purple';
+        break;
+      case 'Drama':
+        return 'pink';
+        break;
+      case 'Family':
+        return 'brown';
+        break;
+      case 'Fantasy':
+        return 'black';
+        break;
+      case 'History':
+        return 'white';
+        break;
+      case 'Horror':
+        return 'grey';
+        break;
+      case 'Music':
+        return 'gold';
+        break;
+      case 'Mystery':
+        return 'silver';
+        break;
+      case 'Romance':
+        return 'violet';
+        break;
     }
+  })
+
+
+// file.map((f, i) => {
+//   svgGraph.append('circle')
+//     .attr('cx', () => x(parseInt(f.Title.substring(f.Title.length - 5, f.Title.length-1))))
+//     .attr('cy', () => y(height-(f['World Sales (in $)'])))
+//     .attr('r', 5)
+//     .attr('fill','indianred')
+// })
+  // renderMovies(file);
+
+async function loadJson(url) {
+  const response = await fetch(url)
+  const parsedJson = await response.json()
+  const posterURL = parsedJson.results[0].poster_path;
+  return posterURL;
 }
 
-/**
- * It creates a new movie list item and appends it to the movie list.
- * @param movieTitle - The title of the movie to be displayed.
- * @param moviePoster - The URL of the movie poster.
- */
+
+async function renderMovies(movies) {
+  for (const movie of movies) {
+    const movieTitle = movie.Title.slice(0, movie.Title.length - 7)
+    const moviePoster = await loadJson(API_QUERY + movieTitle.replaceAll(' ', '+'))
+    renderMovie(movieTitle, moviePoster)
+  }
+}
+
+
 async function renderMovie(movieTitle, moviePoster) {
-    const newMovie = movieListItemTemplate.content.cloneNode(true)
-    newMovie.querySelector('img').setAttribute('src', POSTER_BASE_URL + moviePoster)
-    newMovie.querySelector('p').textContent = movieTitle;
-    movieList.append(newMovie)
-  } 
- 
+  const newMovie = movieListItemTemplate.content.cloneNode(true)
+  newMovie.querySelector('img').setAttribute('src', POSTER_BASE_URL + moviePoster)
+  newMovie.querySelector('p').textContent = movieTitle;
+  movieList.append(newMovie)
+}
+
 const TitlesArray = []
 const IntroductionArray = []
 const StudiosArray = []
--- code/src/loadMovies.js
+++ code/src/loadMovies.js
@@ -1,4 +1,7 @@
-import { tickFormat } from 'd3'
+import {
+  tickFormat
+} from 'd3'
+import * as d3 from 'd3';
 
 
 const TitlesArray = []
@@ -11,91 +14,165 @@ const WorldSalesArray = []
 const GenreArray = []
 const RuntimeArray = []
 
-function getMovies(){
+function getMovies() {
   const Movies = require('../assets/movies.json')
   return Movies
 }
 
-export function getTitles(){
+// export function moviesToCircles(movies) {
+//   //use d3 and create 1 circle per movie colored by genre where y is Worldwide and x is Release Date
+//   d3.select('svg')
+//     .data(movies)
+//     .enter()
+//     .append('circle')
+//     .attr('cx', function (d) {
+//       return x(d.Release / 10)
+//     })
+//     .attr('cy', function (d) {
+//       return y(d.Worldwide / 100000000)
+//     })
+//     .attr('r', function (d) {
+//       return 20;
+//     })
+//     .attr('fill', function (d) {
+      // switch (d.Genre[0]) {
+      //   case 'Action':
+      //     return 'indianred';
+      //     break;
+      //   case 'Adventure':
+      //     return 'blue';
+      //     break;
+      //   case 'Animation':
+      //     return 'green';
+      //     break;
+      //   case 'Comedy':
+      //     return 'yellow';
+      //     break;
+      //   case 'Crime':
+      //     return 'orange';
+      //     break;
+      //   case 'Documentary':
+      //     return 'purple';
+      //     break;
+      //   case 'Drama':
+      //     return 'pink';
+      //     break;
+      //   case 'Family':
+      //     return 'brown';
+      //     break;
+      //   case 'Fantasy':
+      //     return 'black';
+      //     break;
+      //   case 'History':
+      //     return 'white';
+      //     break;
+      //   case 'Horror':
+      //     return 'grey';
+      //     break;
+      //   case 'Music':
+      //     return 'gold';
+      //     break;
+      //   case 'Mystery':
+      //     return 'silver';
+      //     break;
+      //   case 'Romance':
+      //     return 'indigo';
+      //     break;
+      //   case 'Sci-Fi':
+      //     return 'navy';
+      //     break;
+      //   case 'Thriller':
+      //     return 'maroon';
+      //     break;
+      //   case 'War':
+      //     return 'olive';
+      //     break;
+      //   case 'Western':
+      //     return 'lime';
+      //     break;
+      //   default:
+      //     return 'black';
+      // }
+//     })
+// }
+
+export function getTitles() {
   const Movies = getMovies()
-  Movies.map((Movies,i) =>{
+  Movies.map((Movies, i) => {
     TitlesArray.push(Movies.Title)
-  
+
   })
   return TitlesArray
 }
 
-export function getSummary(){
+export function getSummary() {
   const Movies = getMovies()
-  Movies.map((Movies,i) =>{
+  Movies.map((Movies, i) => {
     IntroductionArray.push(Movies['Movie Info'])
-    
+
   })
   return IntroductionArray
 }
 
-export function getStudio(){
+export function getStudio() {
   const Movies = getMovies()
-  Movies.map((Movies,i) =>{
+  Movies.map((Movies, i) => {
     StudiosArray.push(Movies.Distributor)
-    
+
   })
   return StudiosArray
 }
 
-export function getRelease(){
+export function getRelease() {
   const Movies = getMovies()
-  Movies.map((Movies,i) =>{
+  Movies.map((Movies, i) => {
     ReleaseArray.push(Movies['Release Date'])
-    
+
   })
   return ReleaseArray
 }
 
-export function getDomSales(){
+export function getDomSales() {
   const Movies = getMovies()
-  Movies.map((Movies,i) =>{
+  Movies.map((Movies, i) => {
     DomSalesArray.push(Movies['Domestic Sales (in $)'])
-    
+
   })
   return DomSalesArray
 }
 
-export function getIntSales(){
+export function getIntSales() {
   const Movies = getMovies()
-  Movies.map((Movies,i) =>{
+  Movies.map((Movies, i) => {
     IntSalesArray.push(Movies['International Sales (in $)'])
-    
+
   })
   return IntSalesArray
 }
 
-export function getWorldSales(){
+export function getWorldSales() {
   const Movies = getMovies()
-  Movies.map((Movies,i) =>{
+  Movies.map((Movies, i) => {
     WorldSalesArray.push(Movies['World Sales (in $)'])
-    
+
   })
   return WorldSalesArray
 }
 
-export function getGenre(){
+export function getGenre() {
   const Movies = getMovies()
-  Movies.map((Movies,i) =>{
+  Movies.map((Movies, i) => {
     GenreArray.push(Movies.Genre)
-    
+
   })
   return GenreArray
 }
 
-export function getRunTime(){
+export function getRunTime() {
   const Movies = getMovies()
-  Movies.map((Movies,i) =>{
+  Movies.map((Movies, i) => {
     RuntimeArray.push(Movies['Movie Runtime'])
-    
+
   })
   return RuntimeArray
 }
-
-
-
